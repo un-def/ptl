37a25306cca12382064c1a34942fd174d6cf2762
Dmitry Meyer <me@undef.im>
2024-06-08 16:08:37 +0000

    test: add missing positive get_input_dir test

--- tests/infile/test_input_dir.py
+++ tests/infile/test_input_dir.py
@@ -1,9 +1,23 @@
 from pathlib import Path
+from typing import Union
 
 import pytest
 
 from ptl.exceptions import InputDirectoryError
 from ptl.infile import get_input_dir
+
+
+pytestmark = pytest.mark.usefixtures('tmp_cwd')
+
+
+@pytest.mark.parametrize('input_dir_arg', ['dir', Path('dir')])
+def test_passed_path_ok(tmp_cwd: Path, input_dir_arg: Union[str, Path]):
+    path = tmp_cwd / 'dir'
+    path.mkdir()
+
+    input_dir = get_input_dir(input_dir_arg)
+
+    assert input_dir == path
 
 
 def test_passed_path_not_a_dir(tmp_path: Path):
@@ -17,7 +31,7 @@ def test_passed_path_not_a_dir(tmp_path: Path):
 
 
 def test_passed_path_does_not_exist(tmp_path: Path):
-    path = tmp_path / 'requirements'
+    path = tmp_path / 'dir'
 
     with pytest.raises(InputDirectoryError, match=f'{path} does not exist'):
         get_input_dir(path)
@@ -40,7 +54,6 @@ def test_autodiscovery_current_dir(tmp_cwd: Path):
     assert input_dir == tmp_cwd
 
 
-@pytest.mark.usefixtures('tmp_cwd')
 def test_autodiscovery_not_found():
     with pytest.raises(InputDirectoryError, match='input directory not found'):
         get_input_dir()
